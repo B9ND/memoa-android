1784bb9fb4029ec1385eb8d5aadc1e7f663b2901
dlrjsgml44 <leegeh1213@gmail.com>
2024-08-23 15:37:10 +0900

    refactor : Button Shadow

--- app/src/main/java/com/dlrjsgml/memoa/ui/component/Button.kt
+++ app/src/main/java/com/dlrjsgml/memoa/ui/component/Button.kt
@@ -1,6 +1,7 @@
-package com.dlrjsgml.memoa.ui.theme.component
+package com.dlrjsgml.memoa.ui.component
 
 
+import android.graphics.BlurMaskFilter
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -12,14 +13,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.dlrjsgml.memoa.ui.theme.Purple20
-import com.dlrjsgml.memoa.ui.theme.animation.bounceClick
+import com.dlrjsgml.memoa.ui.animation.bounceClick
 import com.dlrjsgml.memoa.ui.theme.caption1
+import com.dlrjsgml.memoa.ui.theme.caption2
 
 
 @Composable
@@ -32,14 +40,19 @@ fun MemoaButton(
     onClick: () -> Unit,
 ) {
     Box(
-        modifier = Modifier.bounceClick(
-            onClick = onClick
-        )
+        modifier = Modifier
+            .bounceClick(
+                onClick = onClick
+            )
+            .dropShadow(
+                shape = shape,
+            )
     ) {
         Box(
             modifier = modifier.background(
                 color = if (enabled) Purple20 else Color.White, shape = shape
             )
+
         ) {
             Text(
                 modifier = Modifier
@@ -60,12 +73,42 @@ fun MemoaButton(
 
 @Preview
 @Composable
-fun MemoaButtonPreview() {
+private fun MemoaButtonPreview() {
     MemoaButton(
         text = "다음",
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 20.dp),
-        contentPadding = PaddingValues(vertical = 20.dp)
+        contentPadding = PaddingValues(vertical = 20.dp),
+        enabled = false
     ) {}
+}
+
+fun Modifier.dropShadow(
+    shape: Shape,
+    color: Color = Color.Black.copy(0.12f),
+    blur: Dp = 14.dp,
+    offsetY: Dp = 10.dp,
+    offsetX: Dp = 0.dp,
+    spread: Dp = 0.dp,
+) = this.drawBehind {
+    val shadowSize = Size(size.width + spread.toPx(), size.height + spread.toPx())
+    val shadowOutline = shape.createOutline(shadowSize, layoutDirection, this)
+
+    val paint = Paint().apply {
+        this.color = color
+    }
+
+    if (blur.toPx() > 0) {
+        paint.asFrameworkPaint().apply {
+            maskFilter = BlurMaskFilter(blur.toPx(), BlurMaskFilter.Blur.NORMAL)
+        }
+    }
+
+    drawIntoCanvas { canvas ->
+        canvas.save()
+        canvas.translate(offsetX.toPx(), offsetY.toPx())
+        canvas.drawOutline(shadowOutline, paint)
+        canvas.restore()
+    }
 }
