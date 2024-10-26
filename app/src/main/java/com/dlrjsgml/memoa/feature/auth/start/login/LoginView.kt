41c7ab7c83ff4253779f8a597f8056b0ca88167c
kmj5004 <kmjmj8769@gmail.com>
2024-10-26 23:13:43 +0900

    feat: singUpScreen

--- app/src/main/java/com/dlrjsgml/memoa/feature/auth/start/login/LoginView.kt
+++ app/src/main/java/com/dlrjsgml/memoa/feature/auth/start/login/LoginView.kt
@@ -42,15 +42,8 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.dlrjsgml.memoa.R
-<<<<<<< HEAD:app/src/main/java/com/dlrjsgml/memoa/feature/login/LoginView.kt
 
-
-import com.dlrjsgml.memoa.feature.SignUp.email.addFocusCleaner
-import com.dlrjsgml.memoa.root.NavGroup
-
-=======
 import com.dlrjsgml.memoa.feature.auth.start.signup.email.addFocusCleaner
->>>>>>> 8d79e96 (feat: singUpScreen):app/src/main/java/com/dlrjsgml/memoa/feature/auth/start/login/LoginView.kt
 import com.dlrjsgml.memoa.ui.component.button.BackButtonWhite
 import com.dlrjsgml.memoa.ui.component.button.MemoaButton
 import com.dlrjsgml.memoa.ui.component.textfield.MemoaTextField
