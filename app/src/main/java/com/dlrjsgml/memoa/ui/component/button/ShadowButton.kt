cd476c2b29cc700bb24a03218c868539d13000db
dlrjsgml44 <leegeh1213@gmail.com>
2024-11-11 18:28:40 +0900

    feat: 설정 페이지

--- app/src/main/java/com/dlrjsgml/memoa/ui/component/button/ShadowButton.kt
+++ app/src/main/java/com/dlrjsgml/memoa/ui/component/button/ShadowButton.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.painterResource
@@ -60,7 +61,7 @@ fun ShadowButton(
     ) {
         Text(modifier = modifier.align(Alignment.CenterVertically), text = text, style = caption1Regular, color = if(text == "로그아웃") Color.Red else Color.Black)
         Spacer(modifier = modifier.weight(1f))
-        MemoaImageButton(onClick = {onClick()}, content = painterResource(id = R.drawable.ic_back))
+        MemoaImageButton(modifier = Modifier.rotate(180F), onClick = {onClick()}, content = painterResource(id = R.drawable.ic_back))
     }
 }
 
