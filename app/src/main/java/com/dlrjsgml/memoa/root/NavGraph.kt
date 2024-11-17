ac21fc99a8c0a3bf5dab2ef5f19bb9977521ed81
dlrjsgml44 <leegeh1213@gmail.com>
2024-11-17 11:52:20 +0900

    fix: 머지에러 고치기

--- app/src/main/java/com/dlrjsgml/memoa/root/NavGraph.kt
+++ app/src/main/java/com/dlrjsgml/memoa/root/NavGraph.kt
@@ -33,7 +33,6 @@ import com.dlrjsgml.memoa.feature.auth.start.signup.email.EmailScreen
 import com.dlrjsgml.memoa.feature.auth.start.signup.name.NameScreen
 import com.dlrjsgml.memoa.feature.auth.start.signup.password.PasswordScreen
 import com.dlrjsgml.memoa.feature.auth.start.signup.schoolchoose.SchoolChooseScreen
-import com.dlrjsgml.memoa.feature.auth.start.signup.schoolchoose.SchoolChooseScreenViewModel
 import com.dlrjsgml.memoa.feature.auth.start.start.StartScreen
 import com.dlrjsgml.memoa.feature.main.bookmark.BookMarkScreen
 import com.dlrjsgml.memoa.feature.main.follower.FollowerScreen
@@ -68,7 +67,7 @@ import com.dlrjsgml.memoa.ui.theme.Black20
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun NavGraph(
-    isLogined :Boolean,
+    isLogined: Boolean,
     navController: NavHostController,
 ) {
 
@@ -170,143 +169,142 @@ fun NavGraph(
             }
         }) { it ->
             NavHost(
-                    modifier = Modifier.padding(it),
-                    navController = navController,
-                    startDestination = if(isLogined) NavGroup.MAIN else NavGroup.START
-                ) {
-                    composable(NavGroup.START) {
-                        StartScreen(navController = navController)
-                    }
-                    composable(NavGroup.LOGIN) {
-                        LoginScreen(navController = navController)
-                    }
-                    composable(NavGroup.SIGNUP_EMAIL) {
-                        EmailScreen(navController = navController)
-                    }
-                    composable("${NavGroup.SIGNUP_PASSWORD}?{email}") {
-                        val email = it.arguments?.getString("email") ?: ""
-                        PasswordScreen(navController = navController, email = email)
-                    }
-                    composable("${NavGroup.SIGNUP_NICKNAME}?{email}?{password}") {
-                        val email = it.arguments?.getString("email") ?: ""
-                        val password = it.arguments?.getString("password") ?: ""
-                        NameScreen(navController = navController, email = email, password = password)
-                    }
-                    composable("${NavGroup.SIGNUP_SCHOOL}?{email}?{password}?{nickname}") {
-                        val email = it.arguments?.getString("email") ?: ""
-                        val password = it.arguments?.getString("password") ?: ""
-                        val nickname = it.arguments?.getString("nickname") ?: ""
-                        SchoolChooseScreen(
-                            navController = navController,
-                            viewModel = SchoolChooseScreenViewModel(),
-                            email = email,
-                            password = password,
-                            nickname = nickname
-                        )
-                    }
-                    composable(NavGroup.SIGNUP_SCHOOL_NOT_FOUND) {
+                modifier = Modifier.padding(it),
+                navController = navController,
+                startDestination = if (isLogined) NavGroup.MAIN else NavGroup.START
+            ) {
+                composable(NavGroup.START) {
+                    StartScreen(navController = navController)
+                }
+                composable(NavGroup.LOGIN) {
+                    LoginScreen(navController = navController)
+                }
+                composable(NavGroup.SIGNUP_EMAIL) {
+                    EmailScreen(navController = navController)
+                }
+                composable("${NavGroup.SIGNUP_PASSWORD}?{email}") {
+                    val email = it.arguments?.getString("email") ?: ""
+                    PasswordScreen(navController = navController, email = email)
+                }
+                composable("${NavGroup.SIGNUP_NICKNAME}?{email}?{password}") {
+                    val email = it.arguments?.getString("email") ?: ""
+                    val password = it.arguments?.getString("password") ?: ""
+                    NameScreen(navController = navController, email = email, password = password)
+                }
+                composable("${NavGroup.SIGNUP_SCHOOL}?{email}?{password}?{nickname}") {
+                    val email = it.arguments?.getString("email") ?: ""
+                    val password = it.arguments?.getString("password") ?: ""
+                    val nickname = it.arguments?.getString("nickname") ?: ""
+                    SchoolChooseScreen(
+                        navController = navController,
+                        viewModel = SchoolChooseScreenViewModel(),
+                        email = email,
+                        password = password,
+                        nickname = nickname
+                    )
+                }
+                composable(NavGroup.SIGNUP_SCHOOL_NOT_FOUND) {
 
-                    }
-                    composable(NavGroup.MAIN) {
-                        MainScreen(navController = navController)
-                    }
-                    composable(route = "${NavGroup.DETAIL}?{phone}",
-                        arguments = listOf(
-                            navArgument("phone") { NavType.StringType }
-                        )) {
+                }
+                composable(NavGroup.MAIN) {
+                    MainScreen(navController = navController)
+                }
+                composable(route = "${NavGroup.DETAIL}?{phone}",
+                    arguments = listOf(
+                        navArgument("phone") { NavType.StringType }
+                    )) {
 
-                        val phoneNum = it.arguments?.getString("phone") ?: ""
-                        DetailScreen(
-                            navController = navController,
-                            boardNumber = phoneNum
-                        )
-                    }
-                    composable(route = "${NavGroup.COMMENT}/phone={phone}",
-                        arguments = listOf(
-                            navArgument("phone") { NavType.StringType }
-                        )) {
-                        val phoneNum = it.arguments?.getString("phone") ?: ""
-                        CommentScreen(
-                            navController = navController,
-                            boardNumber = phoneNum
-                        )
-                    }
-                    composable(route = "${NavGroup.SEARCH}?{search}",
-                        arguments = listOf(
-                            navArgument("search") { NavType.StringType }
-                        )) {
-                        val search = it.arguments?.getString("search") ?: ""
-                        SearchScreen(
-                            navController = navController,
-                            search = search
-                        )
-                    }
-                    composable(NavGroup.BEFORE_SEARCH) {
-                        BeforeSearchScreen(navController = navController)
-                    }
-                    composable(NavGroup.SEARCHING) {
-                        SearchingScreen(navController = navController)
-                    }
-                    composable(NavGroup.WRITE) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                            WriteScreen(navController = navController)
-                        }
-                    }
-                    composable(NavGroup.BOOKMARK) {
-                        BookMarkScreen(navController = navController)
-                    }
-                    composable(route = "${NavGroup.IMAGE_DETAIL}?{phone}",
-                        arguments = listOf(
-                            navArgument("phone") { NavType.StringType }
-                        )) {
-                        val phoneNum = it.arguments?.getString("phone") ?: ""
-                        ImageDetailScreen(
-                            navController = navController,
-                            imgUrl = phoneNum
-                        )
-                    }
-                    composable(NavGroup.PROFILE) {
-                        ProfileScreen(navController)
-                    }
-                    composable(route = "${NavGroup.USERPROFILE}?{phone}",
-                        arguments = listOf(
-                            navArgument("phone") { NavType.StringType }
-                        )) {
-                        val phoneNum = it.arguments?.getString("phone") ?: ""
-                        UserProfileScreen(
-                            navController = navController,
-                            userName = phoneNum
-                        )
-                    }
-
-                    composable(route = "${NavGroup.FOLLOWER}?{phone}?{checker}",
-                        arguments = listOf(
-                            navArgument("phone") { NavType.StringType },
-                            navArgument("checker") { NavType.StringType }
-                        )) {
-                        val phoneNum = it.arguments?.getString("phone") ?: ""
-                        val checkers = it.arguments?.getString("checker") ?: "false"
-                        FollowerScreen(
-                            userId = phoneNum, followingChecker = checkers,
-                            navController = navController
-                        )
-                    }
-                    composable(NavGroup.SETTING) {
-                        SettingScreen(navController)
+                    val phoneNum = it.arguments?.getString("phone") ?: ""
+                    DetailScreen(
+                        navController = navController,
+                        boardNumber = phoneNum
+                    )
+                }
+                composable(route = "${NavGroup.COMMENT}/phone={phone}",
+                    arguments = listOf(
+                        navArgument("phone") { NavType.StringType }
+                    )) {
+                    val phoneNum = it.arguments?.getString("phone") ?: ""
+                    CommentScreen(
+                        navController = navController,
+                        boardNumber = phoneNum
+                    )
+                }
+                composable(route = "${NavGroup.SEARCH}?{search}",
+                    arguments = listOf(
+                        navArgument("search") { NavType.StringType }
+                    )) {
+                    val search = it.arguments?.getString("search") ?: ""
+                    SearchScreen(
+                        navController = navController,
+                        search = search
+                    )
+                }
+                composable(NavGroup.BEFORE_SEARCH) {
+                    BeforeSearchScreen(navController = navController)
+                }
+                composable(NavGroup.SEARCHING) {
+                    SearchingScreen(navController = navController)
+                }
+                composable(NavGroup.WRITE) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        WriteScreen(navController = navController)
                     }
                 }
-                composable(NavGroup.NAME_SETTING){
+                composable(NavGroup.BOOKMARK) {
+                    BookMarkScreen(navController = navController)
+                }
+                composable(route = "${NavGroup.IMAGE_DETAIL}?{phone}",
+                    arguments = listOf(
+                        navArgument("phone") { NavType.StringType }
+                    )) {
+                    val phoneNum = it.arguments?.getString("phone") ?: ""
+                    ImageDetailScreen(
+                        navController = navController,
+                        imgUrl = phoneNum
+                    )
+                }
+                composable(NavGroup.PROFILE) {
+                    ProfileScreen(navController)
+                }
+                composable(route = "${NavGroup.USERPROFILE}?{phone}",
+                    arguments = listOf(
+                        navArgument("phone") { NavType.StringType }
+                    )) {
+                    val phoneNum = it.arguments?.getString("phone") ?: ""
+                    UserProfileScreen(
+                        navController = navController,
+                        userName = phoneNum
+                    )
+                }
+
+                composable(route = "${NavGroup.FOLLOWER}?{phone}?{checker}",
+                    arguments = listOf(
+                        navArgument("phone") { NavType.StringType },
+                        navArgument("checker") { NavType.StringType }
+                    )) {
+                    val phoneNum = it.arguments?.getString("phone") ?: ""
+                    val checkers = it.arguments?.getString("checker") ?: "false"
+                    FollowerScreen(
+                        userId = phoneNum, followingChecker = checkers,
+                        navController = navController
+                    )
+                }
+                composable(NavGroup.SETTING) {
+                    SettingScreen(navController)
+                }
+                composable(NavGroup.NAME_SETTING) {
                     NameSettingScreen(navController)
                 }
-                composable(NavGroup.DESCRIPTION_SETTING){
+                composable(NavGroup.DESCRIPTION_SETTING) {
                     DescriptionScreen(navController)
                 }
             }
 
         }
+
     }
-
-
+}
 
 
 @RequiresApi(Build.VERSION_CODES.O)
